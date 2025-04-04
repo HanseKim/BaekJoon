89ddecbc7e574503a55ee2a9a5cf8095993e3375
HanseKim <kimbird2002@naver.com>
2025-04-04 23:57:27 +0900

    4/4

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
-#include <string>
+#include<iostream>
+#include<queue>
 
 using namespace std;
 
-int solution(string dartResult) {
-    int answer = 0;
-    int num[3] = {0,};
-    for(int i=0;i<dartResult.length();i++){
-        
+int city[21][21] ={{0}};
+
+int main(){
+    int n, c1, c2;
+    cin>>n>>c1>>c2;
+    for ( int i=0;i<n-1;i++){
+        int a,b,l; cin>>a>>b>>l;
+        city[a][b] = l;
+        city[b][a] = l;
     }
-    return answer;
 }
